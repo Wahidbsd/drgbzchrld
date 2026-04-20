27cd5e12df686ac7ce686982d6d7203dd5a68f7b
Wahidbsd <119776622+Wahidbsd@users.noreply.github.com>
2026-04-20 14:10:57 +0100

    Add files via upload

---
+++ api.php
@@ -0,0 +1,60 @@
+{
+    "channels": [
+        {
+            "name": "POSTSPORT1",
+            "url": "https:\/\/tod-plus-viber.rorkhrap90.workers.dev\/live\/postsport1",
+            "headers": {
+                "User-Agent": "Mozilla\/5.0",
+                "Referer": "https:\/\/vibertv.com\/"
+            }
+        },
+        {
+            "name": "POSTSPORT2",
+            "url": "https:\/\/tod-plus-viber.rorkhrap90.workers.dev\/live\/postsport2",
+            "headers": {
+                "User-Agent": "Mozilla\/5.0",
+                "Referer": "https:\/\/vibertv.com\/"
+            }
+        },
+        {
+            "name": "POSTSPORT3",
+            "url": "https:\/\/tod-plus-viber.rorkhrap90.workers.dev\/live\/postsport3",
+            "headers": {
+                "User-Agent": "Mozilla\/5.0",
+                "Referer": "https:\/\/vibertv.com\/"
+            }
+        },
+        {
+            "name": "POSTSPORT4",
+            "url": "https:\/\/tod-plus-viber.rorkhrap90.workers.dev\/live\/postsport4",
+            "headers": {
+                "User-Agent": "Mozilla\/5.0",
+                "Referer": "https:\/\/vibertv.com\/"
+            }
+        },
+        {
+            "name": "POSTSPORT5",
+            "url": "https:\/\/tod-plus-viber.rorkhrap90.workers.dev\/live\/postsport5",
+            "headers": {
+                "User-Agent": "Mozilla\/5.0",
+                "Referer": "https:\/\/vibertv.com\/"
+            }
+        },
+        {
+            "name": "POSTSPORT6",
+            "url": "https:\/\/tod-plus-viber.rorkhrap90.workers.dev\/live\/postsport6",
+            "headers": {
+                "User-Agent": "Mozilla\/5.0",
+                "Referer": "https:\/\/vibertv.com\/"
+            }
+        },
+        {
+            "name": "POSTTV",
+            "url": "https:\/\/tod-plus-viber.rorkhrap90.workers.dev\/live\/posttv",
+            "headers": {
+                "User-Agent": "Mozilla\/5.0",
+                "Referer": "https:\/\/vibertv.com\/"
+            }
+        }
+    ]
+}
